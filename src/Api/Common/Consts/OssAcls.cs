377b8f746bb81af6c8ca93fb89eb56ad1deaff88
沈星繁 <ikesnowy@outlook.com>
2025-05-28 13:32:12 +0800

    feat: add object acl api

---
+++ src/Api/Common/Consts/OssAcls.cs
@@ -0,0 +1,10 @@
+﻿namespace Cuiliang.AliyunOssSdk.Api.Common.Consts
+{
+    public static class OssAcls
+    {
+        public const string PublicReadWrite = "public-read-write";
+        public const string PublicReadOnly = "public-read";
+        public const string PrivateReadWrite = "private";
+        public const string Default = "default";
+    }
+}
